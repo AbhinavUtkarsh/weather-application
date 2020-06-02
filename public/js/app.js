22e65f62c7e227b8280d1c02ba40555bc9ce8093
Abhinav Utkarsh <abhinav.utkarsh@gmail.com>
2020-06-02 21:01:15 +0530

    location added

--- public/js/app.js
+++ public/js/app.js
@@ -5,6 +5,7 @@ const messageTwo = document.querySelector("#message-2")
 const messageThree = document.querySelector("#message-3")
 const messageFour = document.querySelector("#message-4")
 const messageFive = document.querySelector("#message-5")
+const messageSix = document.querySelector("#message-6")
 
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault() //prevents the default submit reload behavior 
@@ -16,6 +17,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageThree.textContent = ""
     messageFour.textContent = ""
     messageFive.textContent = ""
+    messageSix.textContent = ""
     fetch("/weather?address=" + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
@@ -23,6 +25,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageThree.textContent = ""
                 messageFour.textContent = ""
                 messageFive.textContent = ""
+                messageSix.textContent = ""
                 messageTwo.textContent = data.error
             } else {
                 messageTwo.textContent = ""
@@ -30,6 +33,7 @@ weatherForm.addEventListener('submit', (e) => {
                 messageThree.textContent = "Temperature: " + data.currtemp + " degree celsius"
                 messageFour.textContent = "Feels like:  " + data.feelsLikeTemperature + " degree celsius"
                 messageFive.textContent = "Humidity: " + data.humidity + "%"
+                messageSix.textContent = "Location: " + data.location
 
             }
         })
